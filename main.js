5a17d9074e0f622e3747548aa9240b5b9aed205a
Vlad Alexandru Meici <valexand@tibco.com>
2023-02-26 19:25:03 +0200

    26Feb

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
 import { createTimeLine } from './modules/timeline.js'
 
+
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollToPlugin);
+
+// let sections = gsap.utils.toArray(".snap-section");
+
+// function goToSection(panel) {
+//   gsap.to(window, {
+//     scrollTo: {y: panel, autoKill: false, ease: "Power1.easeInOut"},
+//     duration: 0.5
+//   });
+// }
+
+
+
+// sections.forEach((eachPanel, i) => {
+//   // const mainAnim = gsap.timeline({ paused: true });
+
+//   ScrollTrigger.create({
+//     trigger: eachPanel,
+//     onEnter: () => goToSection(eachPanel),
+//     duration: 0.2
+//   });
+
+//   ScrollTrigger.create({
+//     trigger: eachPanel,
+//     start: "bottom bottom",
+//     onEnterBack: () => goToSection(eachPanel),
+//     duration: 0.2
+//   });
+// });
+
+
 window.onload = function () {
   createTimeLine();
+
+  gsap.fromTo(".cloud1", {x: -500},{x: -200, duration: 4,
+    scrollTrigger: {
+      trigger: ".cloud1",
+      toggleActions: 'restart pause restart pause'
+    }
+  });
+  gsap.fromTo(".cloud2", {y: -200},{y: 0, duration: 4,
+    scrollTrigger: {
+      trigger: ".cloud2",
+      toggleActions: 'restart pause restart pause'
+    }
+  });
+  gsap.fromTo(".cloud3", {x: 500},{x: 400, duration: 4,scrollTrigger: {
+    trigger: ".cloud3",
+    toggleActions: 'restart pause restart pause'
+  }});
+  gsap.fromTo(".purpose-content", {opacity: 0},{opacity: 1, duration: 6,scrollTrigger: {
+    trigger: ".purpose-content",
+    toggleActions: 'restart pause restart pause'
+  }});
+
+
+
 
 
   ScrollTrigger.create({
@@ -21,7 +78,7 @@ window.onload = function () {
         trigger: ".trigger",
         start: () => `top+=${i * window.innerHeight} top`,
         end: () => `top+=${(i + 2) * window.innerHeight} top`,
-        markers: true,
+        markers: false,
         scrub: true
       }
     });
